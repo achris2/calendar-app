38a115991795144fac3dd9eda12110d857757eac
Alex Christakou <alex@Alexs-Air-2.broadband>
2023-12-12 23:46:57 +0000

    updated JS

--- script.js
+++ script.js
@@ -1,140 +1,5 @@
-// Your task 
-
-// Display the current day at the top of the calender when a user opens the planner.
-
-
-// Present timeblocks for standard business hours when the user scrolls down.
-
-
-// Color-code each timeblock based on past, present, and future when the timeblock is viewed.
-
-
-// Allow a user to enter an event when they click a timeblock
-
-
-// Save the event in local storage when the save button is clicked in that timeblock.
-
-
-// Persist events between refreshes of a page
-
-
-
-// acceptance criteria 
-// should have an hourly block from 9-5
-// each hour should have a block, with a save button 
-// 1. Display the current day at the top of the calender when a user opens the planner.
-
-
-// 2. Present timeblocks for standard business hours when the user scrolls down.
-
-
-// 3. Color-code each timeblock based on past, present, and future when the timeblock is viewed.
-
-// unavailable times are grey (e.g. past times), current hour is orange, future times are green 
-
-
-// 4. Allow a user to enter an event when they click a timeblock
-
-
-// 5. Save the event in local storage when the save button is clicked in that timeblock.
-
-
-// 6. Persist events between refreshes of a page
-
-// 9 hourly blocks 
-
-
-
-// const today = dayjs().format('DD-MM-YYY');
-
-// const todayData = localStorageData.findIndex(function(element){
-//     return element.date === today; 
-// }); 
-// // populating today data from local storage for different dates 
-// if (todayDataIndex <0){
-//     const todayData = localStorageData[todayDataIndex];
-
-// // show data9ama - data5pm to the text area of each time
-
-// for (let i=9; i<18; i++){
-//     $('#time-block-9').val(data9am);
-
-// document.getElementById('time-block-9').value = data9am; 
-
-
-// }
-
-
-//     // const data 9am = todayData.data[9];
-//     // const data 9am = todayData.data[9];
-//     // const data 9am = todayData.data[9];
-//     // const data 9am = todayData.data[9];
-//     // const data 9am = todayData.data[17];
-
-//     // show data9am till data5pm to the text area of each time
-    
-// }
-
-// how to set the textarea of an item 
-
-// if I name my textarea "time-block-9"
-
-
-// save data to local storage 
-// add click to event listener 
-// in click event listener 
-// get the value of the textarea that correspondes with the button that the user has clicked 
-// iff todayDataIndex exists 
-// ......     const todayData = localStorageData[todayDataIndex];
-//............todayData.data[time] = value of the textarea
-// localstorageData[todayDataIndex] = todayData
-// localstorage.setItem('data', localStorageData);
-// else 
-// // const todayData = {
-//     date:'10-12-2023',
-//     data ()
-// }
-
-
-
-
-///
-
-// 1. 
-// make timeblocks for each hour
-        // div consisting of textarea and save btn
-        // can either hardcode in html or generate with js/jquery
-        // Important: each time block has a unique id or data attribute corresponding to the hour number it is for
-        // ideally that attribute can correspond to the key you use to store the text in local storage
-// 2. 
-// have timeblock colors correspond to past,     present, future
-    // get current time hour via dayjs
-    // loop over all the timeblocks, conditionally assign past present or future class (see css) by comparing that timeblock's hour to the current hour
-//  3. 
- //saving timeblock text in local storage
-    // loop over all the timeblocks and get the current value of the textarea
-    // decide how you want to store the text for each hour in local storage
-    // potential options:
-        // can either store each individual hour as its own key 
-        // (e.g. in local storage, hour-9: "something") 
-        // or store an array objects for each timeblock 
-        // (e.g. [{hour-9: "foo", hour-10: "bar"}])
-// 4. 
-// // retrieve the save values from local storage and display them inside each timeblock whenever the page reloads 
-//     // method depends on how you stored in local storage:
-//         // if you stored as individual keys, get each hour value from local storage and display in the corresponding textarea
-//         // if you stored as an array, loop over that array, get the text for each hour and render it in the corresponding time block
-
-
-
-
-
-
-// insert current day using js.days 
-/* <p id="currentDay" class="lead"></p> */
-
 const currentDate = dayjs().format('dddd, MMMM D, YYYY');
-const currentHour = 10;
+const currentHour = dayjs().hour();
 document.getElementById("currentDay").textContent = currentDate;
 const timeBlocksContainer = document.querySelector('.container');
 var userEntry = JSON.parse(localStorage.getItem("workSchedule"));  
@@ -223,4 +88,10 @@ function updateColours() {
     });
 }
 
+// updates colours 
+
 updateColours();
+
+
+// event listeners for local storage 
+
